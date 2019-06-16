572225ad3c8eb2ddc9951f59d276a123dcca1450
ann-leonard <aleo0779@gmail.com>
2019-06-16 00:01:35 -0400

    change port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var Note = require('./models/note')
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-var PORT = 3000;
+var PORT = 443;
 
 // Initialize Express
 var app = express();
